d55cc2d808db250433a779fad344c893ce6d692b
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-08 16:50:54 -0300

    Implementation of service layer

---
+++ Services/IAuthorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GenericRepository.Models;
+
+namespace GenericRepository.Services
+{
+    public interface IAuthorService
+    {
+        IEnumerable<Author> GetAllAuthors();
+        Task<Author> GetAuthorByName(string firstName);
+        void InsertAuthor(Author author);
+        void UpdateAuthor(Author author);
+        void DeleteAuthor(Guid id);
+    }
+}
---
+++ Services/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GenericRepository.Models;
+
+namespace GenericRepository.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAllBooks();
+        Book GetBookById(Guid bookId);
+        void InsertBook(Book book);
+        void UpdateBook(Book book);
+        void DeleteBook(Guid bookId);
+        Task<Author> CreateSampleBookWithAuthor();
+    }
+}
